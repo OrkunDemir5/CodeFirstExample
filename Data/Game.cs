58fb282758e963a045af991834bfd1c4e5e7a7c3
OrkunDemir5 <orkun.d1999@gmail.com>
2024-12-19 19:26:07 +0300

    Proje dosyası ekle.

---
+++ Data/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExample.Data
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public decimal Rating { get; set; }
+    }
+
+}
---
+++ Data/PatikaFirstDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExample.Data
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=PatikaCodeFirstDb1");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie() { Id = 1, Title = "Inception", Genre = "Sci-Fi", ReleaseYear = 2010 },
+                new Movie() { Id = 2, Title = "The Dark Knight", Genre = "Action", ReleaseYear = 2008 }
+                );
+            modelBuilder.Entity<Game>().HasData(
+                new Game() { Id = 1, Name = "The Witcher 3", Platform = "PC", Rating = 9.8m },
+                new Game() { Id = 2, Name = "The Witcher 3", Platform = "PC", Rating = 9.8m }
+                );
+        }
+    }
+}
